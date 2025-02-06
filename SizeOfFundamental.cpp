eb551adf5c94d280a5030d8caeb2e74bff535174
Jyotirmoy <115767089+Jyotirmoy111@users.noreply.github.com>
2025-02-06 23:59:49 +0530

    Add files via upload

---
+++ SizeOfFundamental.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+int main(){
+    // int a,b,c;
+    
+    cout<<(sizeof(char)) <<"\n";
+    
+    cout<<(sizeof(short)) <<"\n";
+    
+    cout<<(sizeof(int)) <<"\n";
+    
+    cout<<(sizeof(long)) <<"\n";
+    ;
+    cout<<(sizeof(long long)) <<"\n";
+    
+    cout<<(sizeof(float)) <<"\n";
+    
+    cout<<(sizeof(double)) <<"\n";
+    
+    cout<<(sizeof(bool));
+}
